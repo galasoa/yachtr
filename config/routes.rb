436fe84c1fb6ae8ec6159d146d9ce9c06672c8b6
Anthony Passler <passleranthony@yahoo.com>
2020-05-19 11:17:33 +0100

    added profile route, method and view and added monserat and open sans as font for us to use

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'pages/profile', to: "pages#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :yachts do
     resources :bookings, only: [ :new, :create ]
