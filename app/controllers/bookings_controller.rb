22204de75e9a4006e1f16e7270b4f60615587617
camelia svennes <camelia.svennes@gmail.com>
2020-05-19 16:58:35 +0100

    added few spaces in views/pages/profile, and changed few little details in views/yachts/new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,23 @@ class BookingsController < ApplicationController
   def new
   end
 
-  def create
+   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.yacht_id = params[:yacht_id]
+    if @booking.save
+      redirect_to new_booking_path(@booking)
+    else
+      render :new
   end
 end
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:start_date, :end_date)
+    end
+end
+
+
+
