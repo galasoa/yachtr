81afe6ce98125ad7a0f1ab8b7e22a17706185128
Zack Rose <zackrose21@gmail.com>
2020-05-20 13:26:49 +0100

    index cards updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,12 @@ Yacht.destroy_all
 User.destroy_all
 
 locations = ["London","Barbados","Monaco","Cote d'Azur","Marrakesh","Saint Tropez","Sardinia","Tuscany","Florence","Naples"]
+crews = [true,false,true,false,true,false,true,false,true,false]
+capacities = [23,45,65,34,56,98,67,87,69,67]
+prices = [33333,33333,44444,55555,66666,77777,88888,99999,87878]
 User.create(email: "a@a.com", password: "123456")
 
 
 ["Octopus","Luminosity","Barbara","Sunrays","Secret","Lucky Lady","Paraffin","Olivia","Elysian","Blue Eyes London"].each_with_index do |name, index|
-  Yacht.create(name: name, user: User.first, location:locations[index])
+  Yacht.create(name: name, user: User.first, location:locations[index], crew:crews[index], capacity:capacities[index], price:prices[index])
 end
